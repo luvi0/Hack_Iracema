4f567a97533a078b2e3b3b93c9b1ab7cef56727a
Otavio Viana <110121943+luvi0@users.noreply.github.com>
2023-11-19 01:06:13 -0300

    Update import streamlit as st.py

--- import streamlit as st.py
+++ import streamlit as st.py
@@ -2,3 +2,11 @@ import streamlit as st
 
 st.title('Visir 4TAL')
 st.sidebar.title('Menu')
+
+import pandas as pd
+import matplotlib.pyplot as plt
+d_uber = pd.read_csv('uber_fortaleza.csv')
+X = df_uber.iloc[:, 4] #preços
+Y = df_uber.iloc[:, 5] #hora
+plt.scatter(X, Y)
+plt.show()
